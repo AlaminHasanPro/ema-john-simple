3111a50c9ecb9b5f5a84225d3135b37c59553158
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-03-31 11:54:12 +0600

    cart ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Shops from './components/Shops/Shops'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shops></Shops>  
+      
     </div>
   )
 }
